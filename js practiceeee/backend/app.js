e8e08ebc74c012af6bd988596ffd4c0a9ffa46dd
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-01-02 19:24:17 +0530

    app update

--- js practiceeee/backend/app.js
+++ js practiceeee/backend/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
-
+const multer = require('multer');
 const app = express();
+const bodyParser = require('body-parser');
+const tesseract = require('tesseract.js');
+const path = require('path');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
 
 const mongoose = require('mongoose');
 mongoose.set('useUnifiedTopology', 'true' );
@@ -14,19 +19,75 @@ db.once('open', function() {
   // console.log('Connected dude!');
 });
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Headers', true);
-    res.header('Access-Control-Allow-Credentials', 'Content-Type');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    next();
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use('/images', express.static(path.join('backend/images')));
+
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', "*");
+//     res.header('Access-Control-Allow-Headers', true);
+//     res.header('Access-Control-Allow-Credentials', 'Content-Type');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     next();
+// });
+
+// Multer config
+const MIME_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpg',
+    'image/jpg': 'jpg'
+};
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const isValid = MIME_TYPE_MAP[file.mimetype];
+        let error = null;
+        if (!isValid) {
+            error = 'INVALID FILE TYPE!';
+        }
+
+        cb(error, 'backend/images');
+    },
+    filename: (req, file, cb) => {
+        let name = file.originalname.toLowerCase().split(' ').join('-');
+        name = name.substring(0, name.indexOf('.'));
+        const ext = MIME_TYPE_MAP[file.mimetype];
+        cb(null, name + '-' + Date.now() + '.' + ext);
+    }
+});
+
+
 
 app.get("/api/user", (req, res, next) => {
-    console.log(req.query);
     // console.log(req.query.user_name);
-    User.findOne({user_name: req.query.user_name, password: req.query.password}).then(user=>{
+    User.findOne({email: req.query.email, password: req.query.password}).then(user=>{
         res.json({message: "SUCCESS!", user: user});
+    });
+});
+
+app.post("/api/user", (req, res, next) => {
+    User.findOne({ email: req.body.email }).then((ures) => {
+        if (!ures) {
+            return bcrypt.hash(req.body.password, 10).then(hash => {
+                const user = new User({
+                    email: req.body.email,
+                    password: hash
+                });
+                user.save().then(res0=>{
+                    return res.status(201).json({
+                        message: '201K SUCCESS!',
+                        user: res0
+                    });
+                });
+                // console.log(user);
+            }).catch(err => {
+                console.log(err);
+                res.status(500).json({ error: err });
+            });
+        }
+        res.status(403).json({ message: "ALREADY A USER EXIST!" });
     });
 });
 
@@ -36,6 +97,34 @@ app.get("/api/image", (req,res,next)=>{
         res.json(image);
     })
 
+})
+
+app.post("/api/image", multer({storage: storage}).single("image"), (req,res,next) => {
+    const url = req.protocol + '://' + req.get('host');
+    let imagePath = url + '/images/' + req.file.filename;
+    console.log(imagePath);
+
+    tesseract.recognize(
+        imagePath,
+        'eng',
+        { logger: m => console.log(m) }
+      ).then(({ data: { text } }) => {
+        const newImage = Image({
+            user_id: null,
+            image_url: imagePath,
+            img_result: text
+        
+          });
+        newImage.save().then(res0=>{
+            res.json({text: text});
+        });
+      });
+});
+
+app.delete("/api/image/:id", (req,res,next)=>{
+    Image.deleteOne({_id: req.params.id}).then(res0=>{
+        res.json({message: res0});
+    });
 })
 
 module.exports = app;
