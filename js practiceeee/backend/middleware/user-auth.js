9639d96779bcb910ae4434c571fcce0a4fbba4d0
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-01-03 15:01:21 +0530

    updated

--- js practiceeee/backend/middleware/user-auth.js
+++ js practiceeee/backend/middleware/user-auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next)=>{
-    console.log(req.headers);
+    const token = req.headers.authorization.split(" ")[1];
+    console.log(token);
 };
