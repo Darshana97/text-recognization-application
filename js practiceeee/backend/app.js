c19e7199f89e34481c7f348fa032ebfd9916783b
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-01-07 16:20:22 +0530

    updated

--- js practiceeee/backend/app.js
+++ js practiceeee/backend/app.js
@@ -10,14 +10,15 @@ const jwt = require('jsonwebtoken');
 
 const mongoose = require('mongoose');
 mongoose.set('useUnifiedTopology', 'true' );
-mongoose.connect('mongodb://localhost:27017/ocr_application', {useNewUrlParser: true});
+// mongoose.connect('mongodb://localhost:27017/ocr_application', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://darshana:software97@ocr-dgxlc.mongodb.net/ocr?retryWrites=true&w=majority', {useNewUrlParser: true});
 const User = require('../models/user');
 const Image = require('../models/img');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  // console.log('Connected dude!');
+  console.log('Connected dude!');
 });
 
 const checkAuth = require('../backend/middleware/user-auth');
